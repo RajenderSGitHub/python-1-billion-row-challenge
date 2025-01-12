ed995384005888f14b1eeaf1eca08852a02b356f
“RajenderS” <rajender.siriseti@gmail.com>
2025-01-11 22:24:20 -0500

    practice changes

---
+++ calc_polars_practice.py
@@ -0,0 +1,43 @@
+import polars as pl 
+import time
+
+def timeit(func):
+    """
+    Decorator to measure time taken 
+    """
+    def wrapper(*args, **kwargs): 
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f" Time taken processing {func.__name__} is : {(end_time-start_time) * 1000:.2f} ms ")
+        return result
+    return wrapper
+
+
+def _generate_report(df) -> pl.DataFrame: 
+    """
+    Function to add columns avg, max and min temparature by Location
+    """
+    df_result = df.groupby("name").agg(
+                        pl.mean('temparature').alias("avg_temparature"),
+                        pl.min('temparature').alias("min_temparature"),
+                        pl.max('temparature').alias("max_temparature")
+                )
+    return df_result
+
+@timeit
+def main():
+    print(f"main processing begins..")
+    # Read a CSV file
+    df = pl.read_csv("measurements_500.csv")
+    print(f"DataFrame shape: {df.shape}")
+
+    # Generate a report
+    df_result = _generate_report(df)
+    print(f"Result DataFrame: {df_result.shape}")
+    print(df_result.head())
+    df_result.write_csv("output/measurements_500_polars.csv")
+    print(f"Processing complete....")
+
+if __name__ == "__main__":
+    main()
--- data/convertToParquet.py
+++ data/convertToParquet.py
@@ -1,4 +1,16 @@
-import pandas as pd
+import polars as pl
 
-df = pd.read_csv("measurements.txt", sep=";", header=None, names=["station_name", "measurement"], engine="pyarrow")
-df.to_parquet("measurements.parquet")
+# # df = pd.read_csv("data/measurements_small.txt", sep=";", header=None, names=["station_name", "measurement"], engine="pyarrow")
+# df = pl.read_csv("data/measurements_small.txt", sep=";")
+# df.write_csv("measurements_small.csv")
+
+# Define schema explicitly
+schema = {
+    "name": pl.Utf8,  # Column name should be a string (Utf8 in Polars)
+    "temparature": pl.Float64  # Column temparature should be a float (Float64 in Polars)
+}
+
+# Read CSV with the defined schema
+df = pl.read_csv("data/measurements_small.txt", separator=';', schema=schema, has_header=True, ignore_errors=True)
+df = df.drop_nulls()
+df.write_csv("measurements_small.csv")
